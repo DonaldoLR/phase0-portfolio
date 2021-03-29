c6d1a264d976491e0aafecf4241abb37a9f89a3d
github username <github email address>
2021-03-28 20:06:10 -0400

    finished about section

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // console.log('Is this connected?');
+
+const socialMedia = [
+    {company: 'Github', logo: './github-logo.svg', url: 'https://github.com/DonaldoLR'}, 
+    {company: 'Medium', logo: './medium-size.svg', url: 'https://donaldolr.medium.com/'}
+]
+
+for (i = 0; i < socialMedia.length; i++){
+    const socialMediaDiv = document.getElementById('socialMedia-div');
+    let socialIcon = document.createElement('a');
+}
